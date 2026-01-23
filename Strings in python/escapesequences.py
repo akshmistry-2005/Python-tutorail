d7d06669a2b632444edcf07214eaff9c0f51bef7
akshmistry-2005 <akshm205@gmial.com>
2026-01-23 13:13:41 +0530

    Addition in esacapesequence.py

--- Strings in python/escapesequences.py
+++ Strings in python/escapesequences.py
@@ -17,3 +17,14 @@ c1="Here we are learning python. learning python is easy and fun."
 c2="python"
 print(c1.count(c2))
 
+""" Examples 2 """
+h1="here we are using python where where we are learning how the count function works "
+h2="e"
+print(f"The occurence of {h2} is {h1.count(h2)}")
+
+""" Change in the cases of string """
+t1="the change operations of the STRINGS IS DONE LIKE THIS."
+print(t1.upper())
+print(t1.lower())
+print(t1.title())
+print(t1.capitalize())
