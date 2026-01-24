66f44fb29d91a1c16b2b0ad29bcff87c0ab67009
akshmistry-2005 <akshm205@gmial.com>
2026-01-24 13:26:16 +0530

    addition on files

--- Lists in python/lists.py
+++ Lists in python/lists.py
@@ -1,2 +1,8 @@
 studentinfo=["Aksh",20,80.80]
 print(studentinfo)
+""" Lenght of list """
+print(len(studentinfo))
+print(studentinfo[0])
+print(studentinfo[1])
+print(studentinfo[2])
+print(studentinfo[-1])
---
+++ Lists in python/operations on list 2.py
@@ -0,0 +1,23 @@
+"""
+Operations on list part 2:-
+a) extend()
+b) remove()
+c) pop()
+"""
+""" A) extend() """
+fruits=["mango", "grapes","kiwi"]
+print(fruits)
+fruits.extend(["banana","watermelon"])
+print(fruits)
+
+""" B) remove() """
+fruits2=["banana","kiwi","orange","apple"]
+print(fruits2)
+fruits2.remove("orange")
+print(fruits2)
+
+""" C) pop() """
+fruit4=["apple","banana","kiwi","orange","grapes","watermelon","pomogrenate","pear"]
+print(fruit4)
+fruit4.pop(0)
+print(fruit4)
