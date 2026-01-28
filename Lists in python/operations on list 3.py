9ba9410efcd42c9959191a81513e0cd18e62fe16
akshmistry-2005 <akshm205@gmial.com>
2026-01-28 14:54:51 +0530

    Addition of files

---
+++ Lists in python/operations on list 3.py
@@ -0,0 +1,18 @@
+""" OPERATIONS ON STRING PART 3 
+a) reverse()
+b) sort()
+c) count()
+"""
+""" A) REVERSE() """
+days_of_week = ["MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY","SUNDAY"]
+print(days_of_week)
+days_of_week.reverse()
+print(days_of_week)
+
+""" B) SORT() """
+NUMS= [1,2,4,5,6,7,8,9]
+print(NUMS)
+NUMS.sort()
+print("The Sorted list is: ", NUMS)
+NUMS.sort(reverse=True)
+print(NUMS)
