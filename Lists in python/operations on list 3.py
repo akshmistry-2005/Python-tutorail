ec4b5d1a0d058e63c2d8eb57054f5710f8a5c2e6
akshmistry-2005 <akshm205@gmial.com>
2026-02-05 14:28:36 +0530

    Addition of content in existing files(operations on list 3)

--- Lists in python/operations on list 3.py
+++ Lists in python/operations on list 3.py
@@ -2,6 +2,7 @@
 a) reverse()
 b) sort()
 c) count()
+d) membership operation [in]
 """
 """ A) REVERSE() """
 days_of_week = ["MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY","SUNDAY"]
@@ -16,3 +17,15 @@ NUMS.sort()
 print("The Sorted list is: ", NUMS)
 NUMS.sort(reverse=True)
 print(NUMS)
+
+""" C) COUNT() """
+HERE = [1,2,12,122,55,444,56,54,45,45,4,55,66,65,56,12,12,21,222,2,22,11,66,77,88,88,888,8,8,77,77,]
+print(f"The list is: {HERE}")
+ITEM_TO_COUNT= int(input("Enter the number to be counted :"))
+c = HERE.count(ITEM_TO_COUNT)
+print(f"The occurence of {ITEM_TO_COUNT} is : {c} ")
+
+
+""" D) in OPERATION """
+ALPHABETS = ["PYHTON","JAVA","C++","C","KOTLIN","REACT"]
+print("javascrpt" in ALPHABETS)
